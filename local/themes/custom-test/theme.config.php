7f6e94600edfda4e658520eb738281c3b43bb6b9
owenewo1 <owenewo@gmail.com>
2025-09-09 18:23:58 +0000

    Remove unnecessary custom result-list template - contents already display by default

---
+++ local/themes/custom-test/theme.config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'extends' => 'sandal5',  // Bootstrap 5 base theme
+    'css' => [
+        'custom-test.css'
+    ],
+    'js' => [
+        'custom-test.js'
+    ],
+    'favicon' => 'favicon.ico',
+];
